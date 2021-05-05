625d0f6167df17def0eacb5f4d42649ce1af5ee5
ShahonG <ShahonG@csie.io>
2021-05-05 13:36:43 +0800

    second

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const MongoCli = require('mongodb').MongoClient;
+const md5 = require('md5');
 
 const url = "mongodb://127.0.0.1:27017/";
 
 MongoCli.connect(url, (err, db) => {
     if (err) throw err;
-    
     var dbo = db.db("testdb");
-
-    dbo.createCollection("users", (err, res) => {
-        if (err) throw err;
-
-        console.log("Collection created");
-
+    var userInfo = { account: '1234', password: '5678'};
+    userInfo.password = md5(userInfo.password);
+    console.log(userInfo);
+    dbo.collection("users").findOne(userInfo, function(err, result){
+        if (err){
+            console.log("System Failed!");
+            throw err;
+        }
+        if(result == null){
+            console.log("Wrong account or password!\n");
+        }
+        else{
+            console.log("Login Success!\n");
+        }
         db.close();
     });
 });
+
+// LoginInterface(account, password){ };
