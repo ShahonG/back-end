fb575a36d1b476bdc4f1c927b2e06314ea2d3b8a
ShahonG <xianglee0206@gmail.com>
2021-05-05 18:03:48 +0800

    express test

--- server.js
+++ server.js
@@ -1,27 +1,48 @@
+const Mongodb = require('mongodb');
 const MongoCli = require('mongodb').MongoClient;
 const md5 = require('md5');
+const bodyParser = require('body-parser');
+const express = require('express');
 
 const url = "mongodb://127.0.0.1:27017/";
+const app = express();
+var db;
 
-MongoCli.connect(url, (err, db) => {
+MongoCli.connect(url, (err, mongo) => {
     if (err) throw err;
-    var dbo = db.db("testdb");
-    var userInfo = { account: '1234', password: '5678'};
-    userInfo.password = md5(userInfo.password);
-    console.log(userInfo);
-    dbo.collection("users").findOne(userInfo, function(err, result){
-        if (err){
-            console.log("System Failed!");
-            throw err;
-        }
-        if(result == null){
-            console.log("Wrong account or password!\n");
-        }
-        else{
-            console.log("Login Success!\n");
-        }
-        db.close();
-    });
+    db = mongo;
+    app.listen(3000);
+    console.log("Listening on port 3000");
 });
 
-// LoginInterface(account, password){ };
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", function (req, res) {
+    res.send(`
+    <html>
+    <body>
+    <form action="/Login" method="POST">
+    Account : <input type="text" name="account">  <br>
+    Password: <input type="text" name="password">
+    <input type="submit" value="Submit">
+
+    </form>
+    </body>
+    </html>`
+    );
+});
+
+app.post("/Login", function (req, res) {
+    var userInfo = {account:req.body.account, password: md5(req.body.password)};
+    dbo = db.db("testdb");
+    dbo.collection("users").findOne(userInfo, function(err, result){
+        if(err) throw err;
+        if(result == null){
+            res.send("Wrong account or password!");
+            console.log("Wrong account or password!");
+        }
+        else{
+            res.send("Login Success!");
+            console.log("Login Success!");
+        }
+    });
+});
