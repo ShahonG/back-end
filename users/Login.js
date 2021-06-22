e8da924333c8adbd651b921a5254351777ab938d
jeff07014 <gameplayer5032@gmai.com>
2021-06-22 17:29:51 +0800

    修改：     users/Login.js

--- users/Login.js
+++ users/Login.js
@@ -1,39 +1,41 @@
 const router = require("express").Router();
 const passport = require('passport');
 
-router.get("/", (req, res) => {
-    res.send(`
-    <html>
-    <body>
-        <p>Login</p>
-        <form action="/users/Login" method="POST">
-            <div>
-                <label for="account">account</label>
-                <input type="text" id="account "name="account" required>  <br>
-            </div>
-            <div>
-                <label for="password">password</label>
-                <input type="password" id="password "name="password" required>
-            </div>
-            <input type="submit" value="Submit">
+// router.get("/", (req, res) => {
+//     res.send(`
+//     <html>
+//     <body>
+//         <p>Login</p>
+//         <form action="/users/Login" method="POST">
+//             <div>
+//                 <label for="account">account</label>
+//                 <input type="text" id="account "name="account" required>  <br>
+//             </div>
+//             <div>
+//                 <label for="password">password</label>
+//                 <input type="password" id="password "name="password" required>
+//             </div>
+//             <input type="submit" value="Submit">
 
-        </form>
-        <form action="/users/SignUp" method="GET">
-            <input type="submit" value="NewAccount">
-        </form>
-        <form action="/users/OAuth2" method="GET">
-            <input type="submit" value="Sign in with Google">
-        </form>
-    </body>
-    </html>`
-    );
-});
+//         </form>
+//         <form action="/users/SignUp" method="GET">
+//             <input type="submit" value="NewAccount">
+//         </form>
+//         <form action="/users/OAuth2" method="GET">
+//             <input type="submit" value="Sign in with Google">
+//         </form>
+//     </body>
+//     </html>`
+//     );
+//     // console.log(req.session);
+// });
 
 /* local authenticate config in "../config/passport.js" */
 router.post("/", passport.authenticate('local', {
     successRedirect: "/users/Login",
-    failureRedirect: "/users/SignUp",
-    failureFlash: true
+    failureRedirect: "/users/Login",
+    // failureFlash: true
+    failureFlash: "error"
 }));
 
 module.exports = router;
