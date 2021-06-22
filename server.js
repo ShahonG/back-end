4975384ac8ca49f51bed9ecf831872db0bf62ff3
jeff07014 <gameplayer5032@gmai.com>
2021-06-22 21:54:40 +0800

    connect with editor

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ Mongo.connect(url, (err) => {
 var app = express();
 app.set('view engine', 'ejs');
 app.use(express.urlencoded( { extended: false } ));
-// app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 app.use(flash());
 app.use(session({
--- users/Login.js
+++ users/Login.js
@@ -2,41 +2,56 @@ const router = require("express").Router();
 const passport = require('passport');
 
 router.get("/", (req, res) => {
-    res.send('live');
-    // res.send(`
-    // <html>
-    // <body>
-    //     <p>Login</p>
-    //     <form action="/users/Login" method="POST">
-    //         <div>
-    //             <label for="account">account</label>
-    //             <input type="text" id="account "name="account" required>  <br>
-    //         </div>
-    //         <div>
-    //             <label for="password">password</label>
-    //             <input type="password" id="password "name="password" required>
-    //         </div>
-    //         <input type="submit" value="Submit">
+    res.send(`
+    <html>
+    <body>
+        <p>Login</p>
+        <form action="/users/Login" method="POST">
+            <div>
+                <label for="account">account</label>
+                <input type="text" id="account "name="account" required>  <br>
+            </div>
+            <div>
+                <label for="password">password</label>
+                <input type="password" id="password "name="password" required>
+            </div>
+            <input type="submit" value="Submit">
 
-    //     </form>
-    //     <form action="/users/SignUp" method="GET">
-    //         <input type="submit" value="NewAccount">
-    //     </form>
-    //     <form action="/users/OAuth2" method="GET">
-    //         <input type="submit" value="Sign in with Google">
-    //     </form>
-    // </body>
-    // </html>`
-    // );
-    // // console.log(req.session);
+        </form>
+        <form action="/users/SignUp" method="GET">
+            <input type="submit" value="NewAccount">
+        </form>
+        <form action="/users/OAuth2" method="GET">
+            <input type="submit" value="Sign in with Google">
+        </form>
+    </body>
+    </html>`
+    );
+    // console.log(req.session);
 });
 
 /* local authenticate config in "../config/passport.js" */
-router.post("/", passport.authenticate('local', {
-    successRedirect: "/users/Login",
-    failureRedirect: "/users/Login",
-    // failureFlash: true
-    failureFlash: "error"
-}));
+router.post("/",
+    // passport.authenticate('local', {
+    //     successRedirect: "/users/Login",
+    //     failureRedirect: "/users/Login",
+    //     // failureFlash: true
+    //     failureFlash: "error"
+    // })
+    function(req, res) {
+        passport.authenticate('local', function(err, user, info) {
+            if (err) {
+                return 'error';
+            }
+
+            if (user) {
+                res.send("Login Success!");
+            }
+            else {
+                res.send("Login Failed!");
+            }
+        })(req, res);
+    }
+);
 
 module.exports = router;
